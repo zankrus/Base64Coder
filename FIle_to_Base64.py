4afc67fb02c3d114c0f48c2b371899d310e2d014
r.islyamov <sealthepirate@gmail.com>
2020-02-21 14:03:40 +0300

    Version 0.6

--- FIle_to_Base64.py
+++ FIle_to_Base64.py
@@ -1,5 +1,54 @@
 import base64
+import binascii
+from tkinter.filedialog import askopenfilename
+from tkinter import *
 
-with open("01_type7.pdf", "rb") as image_file:
+root = Tk()
+
+
+
+
+
+print("Добро пожаловать в прогу версия 0.6. Роберт Ислямов"  )
+print("Выбери файл, который нужно конвертировать")
+file = askopenfilename()
+print('{:*^60}'.format('Данные'))
+with open(file, "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
-print(encoded_string)
+    encoded_string = str(encoded_string)
+    encoded_string = encoded_string[1:]
+    encoded_string = encoded_string[1:]
+    encoded_string = encoded_string[:-1]
+
+
+for i in range(len(encoded_string)):
+    print(encoded_string[i], end='')
+    i = i + 1
+    if i % 60 == 0:
+        print("")
+
+
+
+
+print("")
+print("")
+print('{:*^60}'.format('Контрольная сумма'))
+
+
+buf = open(file,'rb').read()
+buf = (binascii.crc32(buf) & 0xFFFFFFFF)
+print('{: ^60}'.format(buf))
+
+print("")
+print("")
+
+input('Нажми  ENTER чтобы выйти')
+
+
+
+
+
+
+
+
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 setup(
     name = "Чек=хэш",
-    version = "0.1",
+    version = "0.6",
     description = "Робертоус",
-    executables = [Executable("FIle_to_Base64.py")]
+    executables = [Executable("FIle_to_Base64.py")], requires=['telegram']
     )
